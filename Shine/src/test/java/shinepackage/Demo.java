54c72ac3c42766db8b49e8ff510fbd30987c3c5d
U6075153 <U6075153@U6075153-TPL-A.ten.thomsonreuters.com>
2020-07-02 09:06:34 +0530

    i have copied unstage to stage

---
+++ Shine/src/test/java/shinepackage/Demo.java
@@ -0,0 +1,13 @@
+package shinepackage;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void run()
+	{
+		Reporter.log("yes running",true);
+		Reporter.log("yes runningfggg",true);
+	}
+
+}
