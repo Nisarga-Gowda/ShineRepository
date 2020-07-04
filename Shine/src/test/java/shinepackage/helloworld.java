d9db7aa9d64706b8f53c4adee4821c7b9b113b5f
U6075153 <U6075153@U6075153-TPL-A.ten.thomsonreuters.com>
2020-07-04 08:57:26 +0530

    hello

--- Shine/src/test/java/shinepackage/helloworld.java
+++ Shine/src/test/java/shinepackage/helloworld.java
@@ -1,10 +1,13 @@
 package shinepackage;
 
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class helloworld {
+	
 public void world()
 {
+	Reporter.log("helllo u there",true);
 	Reporter.log("helllo u there",true);
 }
 }
